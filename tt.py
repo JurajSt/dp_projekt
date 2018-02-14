35b361b9384bd078522c46aaf44319be02d1c980
JurajSt <struhar.juraj@gmail.com>
2018-02-14 22:11:37 +0100

    ...

--- tt.py
+++ tt.py
@@ -176,6 +176,8 @@ header, version, headlines, headlength, obstimes, sats, numallsvs, numsvs = scan
 data, obstypes = processBlocks(lines, header, obstimes, headlines, headlength, sats, numallsvs, numsvs)
 sp3 = utility.fReadSP3(path_eph)
 
+del lines, header, version, headlines, headlength
+
 #satellites = ['G01', 'G02', 'G03', 'G04', 'G05', 'G06', 'G07', 'G08', 'G09', 'G11', 'G12', 'G13', 'G14', 'G15', 'G16',
 #              'G17', 'G18', 'G19', 'G20', 'G21', 'G22', 'G23', 'G24', 'G25', 'G26', 'G27', 'G28', 'G29', 'G30', 'G31',
 #              'G32']
